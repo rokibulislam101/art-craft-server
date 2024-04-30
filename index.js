c6f1118f8d29ebd332e49848ded74bdb5b4b6462
MD Rokibul Islam Mollah <rokibulislamalways101@gmail.com>
2024-04-30 11:26:49 +0600

    Important Commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -35,10 +35,24 @@ async function run() {
       res.send(result);
     });
 
+    app.get('/craft/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await craftCollection.deleteOne(query);
+      res.send(result);
+    });
+
     app.post('/craft', async (req, res) => {
       const newCraft = req.body;
       console.log(newCraft)
       const result = await craftCollection.insertOne(newCraft);
+      res.send(result);
+    })
+
+    app.delete('/craft/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await craftCollection.deleteOne(query);
       res.send(result);
     })
 
